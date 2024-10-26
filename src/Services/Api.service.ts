f697d1213d1f9002876051340461ccd66922f374
bhargav5454 <ultracubes7.com>
2024-10-26 10:45:08 +0530

    changed js-api-services into ts-api-services

---
+++ src/Services/Api.service.ts
@@ -0,0 +1,45 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+const API_BASE_URL = "http://localhost:8001/v1";
+
+const getToken = () => Cookies.get("token");
+const getAccessId = () => Cookies.get("xCustomAccessId");
+
+// Create an instance of axios with default settings
+const apiService = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Add a request interceptor to include the token and access ID in every request
+apiService.interceptors.request.use(
+  (config) => {
+    const token = getToken();
+    const accessId = getAccessId();
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    if (accessId) {
+      config.headers["x-custom-access-id"] = accessId;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Generic API call function for get, post, put, delete
+const apiRequest = {
+  get: (url: string) => apiService.get(url),
+  post: (url: string, data: object) => apiService.post(url, data),
+  put: (url: string, data: object) => apiService.put(url, data),
+  delete: (url: string) => apiService.delete(url),
+};
+
+export default apiRequest;
